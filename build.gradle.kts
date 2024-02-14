bf90243bb9ad35becfe717117834e3a6421d702b
Josh Bedwell <rcxwhiz@gmail.com>
2024-02-14 16:48:12 -0600

    Made progress on opening game window
    Need to figure out liblwjgl.dylib

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,23 @@ repositories {
 
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // LWJGL3 bindings
+    val lwjglVersion = "3.3.3"
+    val lwjglNatives = listOf("natives-windows", "natives-linux", "natives-macos", "natives-macos-arm64")  // TODO this should dynamically choose natives?
+
+    // JVM modules
+    val lwjglModules = listOf("glfw", "openal", "opengl", "vulkan")
+    for(module in lwjglModules) {
+        implementation("org.lwjgl:lwjgl-$module:${lwjglVersion}")
+    }
+
+    // Modules that needs native libs
+    val lwjglNativeModules = listOf("glfw", "openal", "opengl")
+    for(module in lwjglNativeModules) {
+        for(native in lwjglNatives)
+            runtimeOnly("org.lwjgl:lwjgl-$module:${lwjglVersion}:$native")
+    }
 }
 
 tasks.test {
