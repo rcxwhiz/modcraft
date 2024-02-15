90e5ff9f82b0d2e95aa56c7eb4efec9d7191f77a
Josh Bedwell <rcxwhiz@gmail.com>
2024-02-15 11:10:07 -0600

    Got gradle configured correctly!

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,33 @@ plugins {
 group = "com.joshbedwell"
 version = "1.0-SNAPSHOT"
 
+val lwjglVersion = "3.3.3"
+val lwjglNatives = Pair(
+    System.getProperty("os.name")!!,
+    System.getProperty("os.arch")!!
+).let { (name, arch) ->
+    when {
+        arrayOf("Linux", "SunOS", "Unit").any { name.startsWith(it) } ->
+            if (arrayOf("arm", "aarch64").any { arch.startsWith(it) })
+                "natives-linux${if (arch.contains("64") || arch.startsWith("armv8")) "-arm64" else "-arm32"}"
+            else if (arch.startsWith("ppc"))
+                "natives-linux-ppc64le"
+            else if (arch.startsWith("riscv"))
+                "natives-linux-riscv64"
+            else
+                "natives-linux"
+        arrayOf("Mac OS X", "Darwin").any { name.startsWith(it) } ->
+            "natives-macos${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+        arrayOf("Windows").any { name.startsWith(it) } ->
+            if (arch.contains("64"))
+                "natives-windows${if (arch.startsWith("aarch64")) "-arm64" else ""}"
+            else
+                "natives-windows-x86"
+        else ->
+            throw Error("Unrecognized or unsupported platform. Please set \"lwjglNatives\" manually.")
+    }
+}
+
 repositories {
     mavenCentral()
 }
@@ -12,27 +39,19 @@ repositories {
 dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // LWJGL3 bindings
-    val lwjglVersion = "3.3.3"
-    val lwjglNatives = listOf("natives-windows", "natives-linux", "natives-macos", "natives-macos-arm64")  // TODO this should dynamically choose natives?
+    implementation(platform("org.lwjgl:lwjgl-bom:$lwjglVersion"))  // TODO idk what this does
 
-    // JVM modules
-    val lwjglModules = listOf("glfw", "openal", "opengl", "vulkan")
-    for(module in lwjglModules) {
-        implementation("org.lwjgl:lwjgl-$module:${lwjglVersion}")
-    }
-
-    // Modules that needs native libs
-    val lwjglNativeModules = listOf("glfw", "openal", "opengl")
-    for(module in lwjglNativeModules) {
-        for(native in lwjglNatives)
-            runtimeOnly("org.lwjgl:lwjgl-$module:${lwjglVersion}:$native")
+    val lwjglModules = listOf("lwjgl", "lwjgl-glfw", "lwjgl-openal", "lwjgl-opengl")  // TODO idk if these are the modules I need
+    for (module in lwjglModules) {
+        implementation("org.lwjgl", module)
+        runtimeOnly("org.lwjgl", module, classifier = lwjglNatives)
     }
 }
 
 tasks.test {
     useJUnitPlatform()
 }
+
 kotlin {
     jvmToolchain(21)
 }
